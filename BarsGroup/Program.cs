6496a68fe5e6cf2c954bcfd035463e3af9b75dc3
gatin <gatin@LAPTOP-GECSPVNA>
2022-03-22 13:47:17 +0300

    test commit

---
+++ BarsGroup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BarsGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
